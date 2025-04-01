fb042fda5ed897912e833e085189b6502eeec246
Piotr Klis <piotr.klis@onewelcome.com>
2025-04-01 10:30:14 +0200

    EXAMPLEAND-132 added empty OmiSdkEngine class

---
+++ app/src/main/java/com/onewelcome/showcaseapp/omisdk/OmiSdkEngine.kt
@@ -0,0 +1,10 @@
+package com.onewelcome.showcaseapp.omisdk
+
+import android.content.Context
+import javax.inject.Inject
+
+class OmiSdkEngine @Inject constructor(context: Context) {
+  fun init() {
+
+  }
+}
--- app/src/main/java/com/onewelcome/showcaseapp/oneginisdk/OneginiSdkEngine.kt
+++
@@ -1,5 +0,0 @@
-package com.onewelcome.showcaseapp.oneginisdk
-
-class OneginiSdkEngine {
-  
-}

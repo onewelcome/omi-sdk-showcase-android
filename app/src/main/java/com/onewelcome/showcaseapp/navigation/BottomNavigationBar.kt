a7431f8957dddef75c987582b8432cf132b974f6
Piotr Klis <piotr.klis@onewelcome.com>
2025-04-02 14:41:14 +0200

    EXAMPLEAND-132 removed redundant import

--- app/src/main/java/com/onewelcome/showcaseapp/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/navigation/BottomNavigationBar.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState

17216c4abaa841e69db229d50b6d0c4ef1366b0c
Aleksander Nowakowski <aleksander.nowakowski@onegini.com>
2025-04-22 11:47:51 +0200

    EXAMPLEAND-152 Apply review remarks

--- app/src/main/java/com/onewelcome/showcaseapp/usecase/IsSdkInitializedUseCase.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/usecase/IsSdkInitializedUseCase.kt
@@ -5,9 +5,5 @@ import com.onewelcome.showcaseapp.core.facade.OmiSdkFacade
 import javax.inject.Inject
 
 class IsSdkInitializedUseCase @Inject constructor(private val omiSdkFacade: OmiSdkFacade) {
-
-  fun execute(): Boolean {
-    return runCatching { omiSdkFacade.oneginiClient }
-      .isOk
-  }
+  fun execute(): Boolean = runCatching { omiSdkFacade.oneginiClient }.isOk
 }
